6621f2f076b8421a54c37295b80fe40676859eaf
Besson Tom <besson.tom@etu.utc.fr>
2022-11-22 15:43:26 +0100

    temp

---
+++ src/fr/utc/pokerut/common/interfaces/client/IHMMainCallsIHMGame.java
@@ -0,0 +1,12 @@
+package src.fr.utc.pokerut.common.interfaces.client;
+
+import src.fr.utc.pokerut.common.dataclass.Game;
+
+public interface IHMMainCallsIHMGame{
+
+    public void createGame(Game new_game){}
+
+    public void newGameWindow(Game gameNewPlayer){}
+
+
+}
