4b538346848405ff44a8660dba3bda994ce35d30
Ludovic Turpin <ludovic.turpin@etu.utc.fr>
2022-11-23 15:55:16 +0100

    Fix interfaces

--- src/fr/utc/pokerut/common/interfaces/client/ComCallsIHMMain.java
+++ src/fr/utc/pokerut/common/interfaces/client/ComCallsIHMMain.java
@@ -2,10 +2,10 @@ package src.fr.utc.pokerut.common.interfaces.client;
 
 import src.fr.utc.pokerut.common.dataclass.*;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ComCallsIHMMain {
     public void displayProfile(ServerProfile serverProfile);
     public void backToMainScreen(int nbCreditFinal, boolean resultat);
+    public boolean notifyRejectionComMainCli(UUID userUuid, UUID gameUuid);
 }
--- src/fr/utc/pokerut/common/interfaces/client/DataCallsIHMMain.java
+++ src/fr/utc/pokerut/common/interfaces/client/DataCallsIHMMain.java
@@ -1,6 +1,5 @@
 package src.fr.utc.pokerut.common.interfaces.client;
 
-import src.fr.utc.pokerut.common.dataclass.ClientProfile;
 import src.fr.utc.pokerut.common.dataclass.Game;
 
 public interface DataCallsIHMMain {
