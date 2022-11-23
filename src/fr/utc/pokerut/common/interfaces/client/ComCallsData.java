8630b941a7f7402944116b3e0bd3539fd442d35d
Fanny Gondouin <fanny.gondouin@etu.utc.fr>
2022-11-23 15:59:38 +0100

    Modification interfaces data

--- src/fr/utc/pokerut/common/interfaces/client/ComCallsData.java
+++ src/fr/utc/pokerut/common/interfaces/client/ComCallsData.java
@@ -1,11 +1,16 @@
 package src.fr.utc.pokerut.common.interfaces.client;
 
+import src.fr.utc.pokerut.common.dataclass.Game;
+import src.fr.utc.pokerut.common.dataclass.Player;
 import src.fr.utc.pokerut.common.dataclass.ServerProfile;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ComCallsData {
     public void connexionUser(ServerProfile profile, String IP, int port);
     public void logoutUser(UUID playerId);
     public void modifyUser(ServerProfile profile);
+    public void addUserAtList(Player player);
+    public void sendLists(List<Player> players, List<Game> games);
 }
