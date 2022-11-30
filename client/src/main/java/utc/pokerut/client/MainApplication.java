15f313f49e96b092ed8c32112fdab40966d5d727
Ludovic Turpin <ludovic.turpin@etu.utc.fr>
2022-11-30 15:02:54 +0100

    Fixes

--- client/src/main/java/utc/pokerut/client/MainApplication.java
+++ client/src/main/java/utc/pokerut/client/MainApplication.java
@@ -1,9 +1,12 @@
 package utc.pokerut.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utc.pokerut.client.ihmmain.controllers.LoginController;
 
 import java.io.IOException;
 
@@ -12,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/mainWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("PokerUT");
         stage.setScene(scene);
         stage.show();
     }
