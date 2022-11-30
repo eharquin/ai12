4fbb12baf3a11b45a122554506099eae0bfb0558
Ludovic Turpin <ludovic.turpin@etu.utc.fr>
2022-11-30 16:19:20 +0100

    Fix tableView

--- client/src/main/java/utc/pokerut/client/ihmmain/controllers/TablesViewController.java
+++ client/src/main/java/utc/pokerut/client/ihmmain/controllers/TablesViewController.java
@@ -1,50 +1,41 @@
 package utc.pokerut.client.ihmmain.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 //import utc.pokerut.common.dataclass.Game;
 import javafx.scene.control.TableView;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
+import utc.pokerut.common.dataclass.Game;
 
 
 public class TablesViewController {
-    //@FXML private TableView<Game> tableView;
-    @FXML private TextField nameField;
-    @FXML private TextField creatorField;
-    @FXML private TextField nbToursFiled;
-    @FXML private TextField betField;
-    @FXML private TextField playersField;
-    @FXML private TextField statusField;
-
+    @FXML
+    private TableView<Game> myTableView;
 
     @FXML
-    private void joinGame() throws IOException {
-       // addUserToGameDataMainClient(gameNewPlayer, newPlayer, idUser);
-        //displayGame(newGame);
+    private TableColumn<Game, String> name;
+
+    public void initialize() {
+        name.setCellValueFactory(new PropertyValueFactory<Game, String>("name"));
+
+        myTableView.getItems().setAll(getItemsToAdd());
     }
 
-    @FXML
-    private void createGame() throws IOException {
-    
-    }
-
-    //construction of the Tables list to display in the board 
-    @FXML
-    protected void addTable(ActionEvent event) {
-        //quelle action faire (ini, ajout, remove?)
-        //récupérer tous les games disponibles sur ce serveur dans une liste
-        
-        //pour chaque game current de cette liste:
-    
-            /*nameField.setText(current.getName());
-            creatorField.setText(current.getCreator());
-            nbToursField.setText(current.getNbTours());   
-            betField.setText(current.getCrruentBet());
-            playersField.setText(current.getPlayers());
-            statusField.setText(current.getStatus()); */  
-        
+    private List<Game> getItemsToAdd(){
+        List<Game> list = new ArrayList<>();
+        Game game1 = new Game();
+        game1.setName("Bob");
+        list.add(game1);
+        System.out.println(game1.getName());
+        return list;
     }
 
     
