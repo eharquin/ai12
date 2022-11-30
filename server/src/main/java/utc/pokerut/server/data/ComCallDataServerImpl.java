0c489cc4f991fbd09bc7744d3ea6f3b8a8de5f57
lblondel <lidia.blondel@etu.utc.fr>
2022-11-30 16:11:16 +0100

    Corps des classes dataserver

---
+++ server/src/main/java/utc/pokerut/server/data/ComCallDataServerImpl.java
@@ -0,0 +1,72 @@
+package utc.pokerut.server.data;
+
+import utc.pokerut.common.dataclass.*;
+import utc.pokerut.common.interfaces.server.ComCallsData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ComCallDataServerImpl implements ComCallsData {
+    private Core dataServerCore;
+
+    @Override
+    public void sendUpdateRound(Round round, List<UUID> Players) {
+
+    }
+
+    @Override
+    public void sendUpdateNextRound(Round round, List<UUID> players) {
+
+    }
+
+    @Override
+    public void sendNextPlayerActions(List<Action> actions, UUID nextPlayer) {
+
+    }
+
+    @Override
+    public void sendUserActionRefused() {
+
+    }
+
+    @Override
+    public void sendUpdateRoundAndNewRound(Round round, Round newRound, List<UUID> players) {
+
+    }
+
+    @Override
+    public void transmitLeaveMessage(UUID idPlayer, UUID idGame, int nbCreditFinal, boolean result) {
+
+    }
+
+    @Override
+    public void joinTableRequestDataComServ(UUID idUser, UUID idGame) {
+
+    }
+
+    @Override
+    public void addUserToGameDataComServ(Game gameNewPlayer, Player newPlayer, UUID idUser) {
+
+    }
+
+    @Override
+    public void modify(ServerProfile profile) {
+
+    }
+
+    @Override
+    public void sendUpdateRoundAndEndResult(Round round, ArrayList<Result> results, List<UUID> players) {
+
+    }
+
+    @Override
+    public void sendNewRound(Round round, Round newRound, List<UUID> players) {
+
+    }
+
+    @Override
+    public void initGameServer(Game newGame) {
+        dataServerCore.getWaitingGames().add(newGame);
+    }
+}
--- server/src/main/java/utc/pokerut/server/data/Core.java
+++ server/src/main/java/utc/pokerut/server/data/Core.java
@@ -1,4 +1,54 @@
 package utc.pokerut.server.data;
 
+import utc.pokerut.common.dataclass.Game;
+import utc.pokerut.common.dataclass.ServerProfile;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class Core {
+
+
+    // private ComCallDataServerImpl iComServerCallDataServerImpl;
+    //private DataServerCallComServer iDataServerCallComServer; // à déclarer dans common/interface
+    private ArrayList<GameEngine> onGoingGames;
+    private ArrayList<Game> waitingGames;
+    private ArrayList<ServerProfile> connectedPlayers;
+
+    public Game getOnGoingGame(UUID gameId) {
+        Game game = onGoingGames.stream().filter(a -> a.getGame().getId() == gameId).collect(Collectors.toList()).get(0).getGame();
+        return game;
+    }
+
+    public GameEngine getOnGoingGameEngine(UUID gameId) {
+        GameEngine game = onGoingGames.stream().filter(a -> a.getGame().getId() == gameId).collect(Collectors.toList()).get(0);
+        return game;
+    }
+
+
+    public ArrayList<GameEngine> getOnGoingGames() {
+        return onGoingGames;
+    }
+
+    public void setOnGoingGames(ArrayList<GameEngine> onGoingGames) {
+        this.onGoingGames = onGoingGames;
+    }
+
+    public ArrayList<Game> getWaitingGames() {
+        return waitingGames;
+    }
+
+    public void setWaitingGames(ArrayList<Game> waitingGames) {
+        this.waitingGames = waitingGames;
+    }
+
+    public ArrayList<ServerProfile> getConnectedPlayers() {
+        return connectedPlayers;
+    }
+
+    public void setConnectedPlayers(ArrayList<ServerProfile> connectedPlayers) {
+        this.connectedPlayers = connectedPlayers;
+    }
+
 }
