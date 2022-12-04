522bd4f940fe98e27ca5d8500d38ab0dfd784f89
lblondel <lidia.blondel@etu.utc.fr>
2022-12-04 15:10:32 +0100

    Add the methods body for combination evaluations

--- server/src/main/java/utc/pokerut/server/data/GameEngine.java
+++ server/src/main/java/utc/pokerut/server/data/GameEngine.java
@@ -1,9 +1,11 @@
 package utc.pokerut.server.data;
 
+import utc.pokerut.common.dataclass.Card;
 import utc.pokerut.common.dataclass.Game;
 import utc.pokerut.common.dataclass.Result;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameEngine {
     private Game game;
@@ -40,5 +42,43 @@ public class GameEngine {
     public boolean isCallingPossible(){
         return true;
     }
-}
 
+    public List<Card> sortCards(List<Card> cardList){
+        List<Card> cards = null;
+        // ordre croissant des valeurs, peu importe les symboles
+        return cards;
+    }
+
+    public boolean isQuinte(List<Card> cardList){
+        //vérifie qui +1 entre chaque card.value
+        return true;
+    }
+
+    public boolean isQuinteFlush(List<Card> cardList){
+        //vérifie que même symbol sur ttes les cards
+        return true;
+    }
+
+    public boolean isQuinteFlushRoyale(List<Card> cardList){
+        //vérifie que max = as/min = 10
+        return true;
+    }
+    public void getMultiple(List<Card> cardList){
+        // hmap pour les multiples
+        /* pour une combinaison, tant qu'il y a des cartes à parcourir*/
+        int nbCards = 5;
+        int initValue = 0 ; // mettre value première carte
+
+        while (nbCards > 0){ // nbCards-=1 après parcours
+            /* while carte suivante a la même value que initValue
+                   ajouter 1 à la multiplicité de la carte
+             */
+        }
+        // retourne le ou les multiples avec la valeur associée
+    }
+
+    public boolean isSameColor(List<Card> cardList){
+        //toutes les cartes ont le même attribut symbol
+        return true;
+    }
+}
