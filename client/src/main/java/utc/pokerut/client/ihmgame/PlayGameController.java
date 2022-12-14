41b1440c8f0f9c380444fd036af293b492336a00
doualecl <clement.douale@etu.utc.fr>
2022-12-14 13:50:37 +0100

    :construction: Méthodes des actions de jeu créées. Besoin des implémentations de data et com pour appeler correctement les interfaces et objets

--- client/src/main/java/utc/pokerut/client/ihmgame/PlayGameController.java
+++ client/src/main/java/utc/pokerut/client/ihmgame/PlayGameController.java
@@ -3,13 +3,21 @@ package utc.pokerut.client.ihmgame;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import utc.pokerut.client.MainApplication;
+import utc.pokerut.client.ihmmain.controllers.PlayerListController;
+import utc.pokerut.client.ihmmain.listeners.PlayerListListener;
+import utc.pokerut.common.dataclass.Action;
+import utc.pokerut.common.dataclass.ActionTypeEnum;
 import utc.pokerut.common.dataclass.Game;
+import utc.pokerut.client.ihmgame.Core;
 
 import javafx.scene.control.TextField;
 import java.io.IOException;
+import java.security.Timestamp;
+import java.util.UUID;
 
 public class PlayGameController {
 
@@ -23,6 +31,7 @@ public class PlayGameController {
 
     protected  static Core core;
 
+
     @FXML
     public TextField betting;
     @FXML
@@ -34,27 +43,68 @@ public class PlayGameController {
         Stage stage = new Stage();
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
+
     }
     @FXML
     public void RaiseValue(ActionEvent event){
         event.consume();
         System.out.println(raising.getText());
+        final Node source = (Node) event.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
         // Call Data : montant de la relance retiré de ses crédits et ajouté au pot
 
+        // Calling round to get player
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.RAISE);
+        action.setBetting(Integer.parseInt(raising.getText()));
+        // action.setTimestamp(new Timestamp());
+
+        // sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
     }
     @FXML
     public void Call(ActionEvent event){
+
         // Call Data : montant de la mise retiré de ses crédits et ajouté au pot
         // Si pas dernier joueur et pas eu de relance -> fin du tour ? Est-ce à nous de gérer ça?
         // go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.CALL);
+        // Récupérer la mise du round en cours Int mise = ...
+        // action.setBetting(mise);
+        // action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+
     }
     @FXML
     public void Check(ActionEvent event){
         // Inform Data, go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.CHECK);
+        // Récupérer la mise du round en cours Int mise = ...
+        action.setBetting(0);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
     }
     @FXML
-    public void Pass(ActionEvent event){
+    public void Fold(ActionEvent event){
         // Inform Data, go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.FOLD);
+        // Récupérer la mise du round en cours Int mise = ...
+        action.setBetting(0);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
     }
 
     @FXML
@@ -68,8 +118,18 @@ public class PlayGameController {
     public void BetValue(ActionEvent event){
         event.consume();
         System.out.println(betting.getText());
-
+        final Node source = (Node) event.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
         // Calling Data with the value
+        Action action = new Action();
+       // Action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+       // action.setType(ActionTypeEnum.); // Demander implémentation de BET à DATA
+        // Récupérer la mise du round en cours Int mise = ...
+       // action.setBetting(mise);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
 
     }
 
