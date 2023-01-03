99c66a478012fc8565fb6c3a949c4a8d7bc057df
pgrelier <pierre-louis.grelier@etu.utc.fr>
2023-01-03 21:26:25 +0100

    correction des beugs pour la partie Card

--- client/src/main/java/utc/pokerut/client/ihmgame/MappingImage.java
+++ client/src/main/java/utc/pokerut/client/ihmgame/MappingImage.java
@@ -3,14 +3,15 @@ package utc.pokerut.client.ihmgame;
 import javafx.scene.image.Image;
 import utc.pokerut.client.MainApplication;
 import javafx.fxml.FXML;
+import utc.pokerut.common.dataclass.Card;
 
 public class MappingImage {
 
     //on récupère l'objet image de data
     public Card card;
 
-    public Image getCard() {
-        return card;
+    public Card getCard() {
+        return this.card;
     }
 
     public void setCard(Card card) {
@@ -19,7 +20,7 @@ public class MappingImage {
 
     //Méthode de mapping entre le lien url de l'image et l'image stockée dans le code
     public void mapping (Card card) {
-        Image playerCard = new Image(String.valueOf(MainApplication.class.getResource("img/ihmgame/"+card.color+"/"+card.value+".png")));
+        Image playerCard = new Image(String.valueOf(MainApplication.class.getResource("img/ihmgame/"+card.getSymbol()+"/"+card.getValue()+".png")));
     }
 
 }
