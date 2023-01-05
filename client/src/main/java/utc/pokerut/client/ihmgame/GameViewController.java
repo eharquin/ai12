b2cef2ce76ee51fb45622742dcac2ad9eac83b59
doualecl <clement.douale@etu.utc.fr>
2023-01-05 20:12:20 +0100

    :construction: fix vue avec bouton mergée sur vue principale, méthodes des controllers aussi, modification vue avec quelques ajustements pour la place des boutons.

--- client/src/main/java/utc/pokerut/client/ihmgame/GameViewController.java
+++ client/src/main/java/utc/pokerut/client/ihmgame/GameViewController.java
@@ -1,18 +1,25 @@
 package utc.pokerut.client.ihmgame;
 
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import utc.pokerut.client.MainApplication;
 import utc.pokerut.client.ihmgame.Core;
+import utc.pokerut.client.ihmmain.controllers.PlayerListController;
+import utc.pokerut.common.dataclass.Action;
+import utc.pokerut.common.dataclass.ActionTypeEnum;
 import utc.pokerut.common.dataclass.Game;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -30,6 +37,11 @@ public class GameViewController {
     private ImageView[][] playerCardsImageArray;
 
     private ImageView[] centerCardsImageArray;
+
+    @FXML
+    public TextField betting;
+    @FXML
+    public TextField raising;
 
     public GameViewController(){
         this.initImageViewPointers();
@@ -675,5 +687,107 @@ public class GameViewController {
         Stage stage = new Stage();
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();}*/
+
+    @FXML
+    public void Raise(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/RaiseAction.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
+
+    }
+    @FXML
+    public void RaiseValue(javafx.event.ActionEvent event){
+        event.consume();
+        System.out.println(raising.getText());
+        final Node source = (Node) event.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        // Call Data : montant de la relance retiré de ses crédits et ajouté au pot
+
+        // Calling round to get player
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.RAISE);
+        action.setBetting(Integer.parseInt(raising.getText()));
+        // action.setTimestamp(new Timestamp());
+
+        // sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+    }
+    @FXML
+    public void Call(javafx.event.ActionEvent event){
+
+        // Call Data : montant de la mise retiré de ses crédits et ajouté au pot
+        // Si pas dernier joueur et pas eu de relance -> fin du tour ? Est-ce à nous de gérer ça?
+        // go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.CALL);
+        // Récupérer la mise du round en cours Int mise = ...
+        // action.setBetting(mise);
+        // action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+
+    }
+    @FXML
+    public void Check(javafx.event.ActionEvent event){
+        // Inform Data, go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.CHECK);
+        // Récupérer la mise du round en cours Int mise = ...
+        action.setBetting(0);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+    }
+    @FXML
+    public void Fold(javafx.event.ActionEvent event){
+        // Inform Data, go to next player
+        event.consume();
+        Action action = new Action();
+        action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        action.setType(ActionTypeEnum.FOLD);
+        // Récupérer la mise du round en cours Int mise = ...
+        action.setBetting(0);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+    }
+
+    @FXML
+    public void Bet(javafx.event.ActionEvent event ) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/BetAction.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
+    }
+
+    public void BetValue(ActionEvent event){
+        event.consume();
+        System.out.println(betting.getText());
+        final Node source = (Node) event.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        // Calling Data with the value
+        Action action = new Action();
+        // Action.setPlayer(PlayerListController.getCore().getPlayerConnected());
+        // action.setType(ActionTypeEnum.); // Demander implémentation de BET à DATA
+        // Récupérer la mise du round en cours Int mise = ...
+        // action.setBetting(mise);
+        //action.setTimestamp(new Timestamp());
+
+        //sendAction(PlayerListController.getCore().getPlayerConnected().getId(), getCore().getGame().getId(), action);
+
+    }
+
+    public void updatePlayers(Game game){
+        // load every player in the game
+        // if player leave, update view
+    }
+
 
 }
